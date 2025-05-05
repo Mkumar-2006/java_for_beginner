a6aa68f40827bb651118c4b9dda67dcacea9bef3
Mkumar-2006 <kumarmohit12262@gmail.com>
2025-05-05 11:29:34 +0530

    first message

--- Arraysearch.java
+++ Arraysearch.java
@@ -7,17 +7,29 @@ public class Arraysearch {
         int[] arr = {1,2,3,4,5,6,7,8};
         System.out.print("Enter the number you want to search : ");
         int num = input.nextInt();
+        input.close();
+        boolean isFound = isFound(arr, num);
+        if(isFound){
+            System.out.println("Your number was found in the array");
+        }
+        else{
+            System.out.println("Your number was not found in the array");
+        }
+    
     }
-
+        
 
     public static boolean isFound(int[] arr,int num){
         int i = 0;
-        while()
-        if(int[] arr == nu)
+        while(i<arr.length){
+        if(arr[i] == num){
+        return true;
+    }
+        i++;
+}
         return false;
 
     }
+
 }
-
-
 // C:\Users\Dell Pc\Desktop\CODING\JAVA
